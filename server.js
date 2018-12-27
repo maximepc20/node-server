788bfe45dc65925466d878062085c116fcbfd214
Maxime <max_pc20@hotmail.com>
2018-12-27 13:31:18 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const hbs     = require('hbs');
+const fs      = require('fs')
+
+const portNumber = 3000;
+const maintenance = false;
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log', log + '\n', (err) => {
+        console.log('Unable to append to server: ', err);
+    });
+    console.log(log);
+    next();
+});
+
+app.use((req, res, next) => {
+    if(maintenance)
+    {
+        res.render('maintenance.hbs', { });
+    }
+    else {
+        next();
+    }
+});
+
+app.use(express.static(__dirname+ '/public_html'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear() ;
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    // res.send('<h1>Hello Express</h1>');
+    res.render('home.hbs', {
+        pageTitle : 'Home page',
+        pageMessage: 'Welcome to my website'
+    });
+});
+
+app.get('/json', (req, res) => {
+    // res.send('<h1>Hello Express</h1>');
+    res.send({
+        name: 'Maxime',
+        likes: [
+            'Kayak',
+            'Piano'
+        ]
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle : 'About page',
+        pageMessage: 'About this website'
+    });
+})
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Oups! There\'s no page here'
+    });
+})
+
+app.listen(portNumber, () => {
+    console.log('Server is up on port ', portNumber);
+});
